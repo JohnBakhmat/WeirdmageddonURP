bd9476766ed046adf36bf09c62588db48a81e3a4
John Bakhmat <yevhenii.bakhmat@nure.ua>
2023-02-07 13:13:48 +0300

    rename item sprite to icon

--- Assets/Scripts/Gameplay/Abstract/Item.cs
+++ Assets/Scripts/Gameplay/Abstract/Item.cs
@@ -5,7 +5,7 @@ public abstract class Item : Interactable
   public abstract void Use();
   public abstract void Drop();
 
-  public Sprite sprite = null;
+  public Sprite icon = null;
 
   public override void Interact(Player player)
   {
