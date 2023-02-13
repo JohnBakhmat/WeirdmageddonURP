e90bec19a3c27a826a5cf79427320a9a4f1297c4
John Bakhmat <yevhenii.bakhmat@nure.ua>
2023-02-13 18:51:33 +0300

    Comment camera action code

--- Assets/Scripts/Gameplay/Items/CameraItem.cs
+++ Assets/Scripts/Gameplay/Items/CameraItem.cs
@@ -10,6 +10,7 @@ public class CameraItem : Item
 
 
   [SerializeField][Range(0, 180)] private float rayAngle = 20f;
+  [SerializeField][Range(0, 10)] private float rayLength = 5f;
 
 
   protected override void Action(Player player)
@@ -21,17 +22,34 @@ public class CameraItem : Item
     var target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     var direction = target - start;
 
-
-    var x = direction.x;
-    var y = direction.y;
-
+    direction = direction.normalized * rayLength;
 
     var topVector = Quaternion.AngleAxis(rayAngle, Vector3.forward) * direction;
     var bottomVector = Quaternion.AngleAxis(-rayAngle, Vector3.forward) * direction;
 
-    Debug.DrawRay(start, topVector, Color.red, 1f);
-    Debug.DrawRay(start, direction, Color.green, 1f);
-    Debug.DrawRay(start, bottomVector, Color.blue, 1f);
+    // var topRay = Physics2D.Raycast(start, topVector, rayLength, LayerMask.GetMask("Walls"));
+    // var bottomRay = Physics2D.Raycast(start, bottomVector, rayLength, LayerMask.GetMask("Walls"));
+    // var centerRay = Physics2D.Raycast(start, direction, rayLength, LayerMask.GetMask("Walls"));
+
+    // var rays = new List<RaycastHit2D> { topRay, bottomRay, centerRay };
+
+    // foreach (var ray in rays)
+    // {
+    //   if (ray.collider != null)
+    //   {
+    //     var hitpoint = ray.point;
+
+    //     var mark = new GameObject("Mark");
+    //     mark.transform.position = hitpoint;
+    //     mark.AddComponent<SpriteRenderer>().sprite = GetComponent<SpriteRenderer>().sprite;
+
+    //   }
+    // }
+
+
+    // Debug.DrawRay(start, topVector, Color.red, 0.5f);
+    // Debug.DrawRay(start, direction, Color.blue, 0.5f);
+    // Debug.DrawRay(start, bottomVector, Color.red, 0.5f);
 
   }
 
