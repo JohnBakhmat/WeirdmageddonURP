64b1b178fd0c4a0cd440b7c8601ca885f9425345
John Bakhmat <yevhenii.bakhmat@nure.ua>
2023-02-13 14:01:57 +0300

    Remove the log

--- Assets/Scripts/Gameplay/Items/CameraItem.cs
+++ Assets/Scripts/Gameplay/Items/CameraItem.cs
@@ -8,12 +8,6 @@ public class CameraItem : Item
     Debug.Log("CameraItem dropped");
   }
 
-  public override void Interact(Player player)
-  {
-    base.Interact(player);
-    Debug.Log("CameraItem picked up");
-  }
-
   protected override void Action(Player player)
   {
     Debug.Log("CameraItem used");
