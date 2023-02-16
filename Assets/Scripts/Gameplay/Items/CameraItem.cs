7e834ff39b86045902402566777145ccee82b955
John Bakhmat <yevhenii.bakhmat@nure.ua>
2023-02-16 12:43:57 +0300

    Made camera light follow mouse

--- Assets/Scripts/Gameplay/Items/CameraItem.cs
+++ Assets/Scripts/Gameplay/Items/CameraItem.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class CameraItem : Item
 {
@@ -11,6 +13,41 @@ public class CameraItem : Item
 
   [SerializeField][Range(0, 180)] private float rayAngle = 20f;
   [SerializeField][Range(0, 10)] private float rayLength = 5f;
+
+  [Header("Light")]
+  [SerializeField][Range(0, 15)] private float flashIntencity = 0.8f;
+
+  [SerializeField] private GameObject lightSource;
+
+  protected override void Update()
+  {
+    base.Update();
+    TrackMouse();
+  }
+
+  private void TrackMouse()
+  {
+    var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    var lightPos = lightSource.transform.position;
+
+    var dif = mousePos - lightPos;
+    var sign = (dif.x > 0) ? -1 : 1;
+    var a = new Vector2(dif.x, dif.y);
+    var angle = Vector2.Angle(Vector2.up, a) * sign;
+    var rotationVector = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+    lightSource.transform.rotation = rotationVector;
+  }
+
+  private IEnumerator FlashCoroutine()
+  {
+    var light = lightSource.GetComponent<Light2D>();
+
+    light.intensity = flashIntencity;
+
+    yield return new WaitForSeconds(0.1f);
+
+    light.intensity = 0;
+  }
 
 
   protected override void Action(Player player)
@@ -26,6 +63,9 @@ public class CameraItem : Item
 
     var topVector = Quaternion.AngleAxis(rayAngle, Vector3.forward) * direction;
     var bottomVector = Quaternion.AngleAxis(-rayAngle, Vector3.forward) * direction;
+
+    StartCoroutine(FlashCoroutine());
+
 
     // var topRay = Physics2D.Raycast(start, topVector, rayLength, LayerMask.GetMask("Walls"));
     // var bottomRay = Physics2D.Raycast(start, bottomVector, rayLength, LayerMask.GetMask("Walls"));
