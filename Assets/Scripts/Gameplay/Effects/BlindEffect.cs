3c7923d8be8a3cc0f49ff4984bca0359853d31ed
John Bakhmat <yevhenii.bakhmat@nure.ua>
2023-02-21 11:09:56 +0300

    Blindness effect vignette

--- Assets/Scripts/Gameplay/Effects/BlindEffect.cs
+++ Assets/Scripts/Gameplay/Effects/BlindEffect.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 [CreateAssetMenu(fileName = "Blind Effect", menuName = "ScriptablObjects/Effects/BlindEffect", order = 1)]
 public class BlindEffect : Effect
@@ -7,8 +9,19 @@ public class BlindEffect : Effect
   {
   }
 
+
+
+  private Vignette vignette;
+
   public override void EffectTick()
   {
-    Debug.Log($"Blind effect tick {((int)timeLeft).ToString()}");
+
+    GameObject.Find("PostProcessingVolume")
+    .GetComponent<Volume>()
+    .profile.TryGet<Vignette>(out vignette);
+
+    var intensity = timeLeft / duration;
+    vignette.intensity.value = Mathf.Lerp(0, 0.9f, intensity);
+
   }
 }
