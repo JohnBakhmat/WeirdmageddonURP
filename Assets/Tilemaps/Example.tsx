2dfad9f2e66d1ddd7ce8286efb2a95c38547bf9c
John Bakhmat <yevhenii.bakhmat@nure.ua>
2023-02-22 12:57:47 +0300

    Fixed some tilemap collisions and introduced slopes

--- Assets/Tilemaps/Example.tsx
+++ Assets/Tilemaps/Example.tsx
@@ -1,4 +1,30 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Example" tilewidth="16" tileheight="16" tilecount="80" columns="10">
- <image source="Tilemap.png" width="173" height="128"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Example" tilewidth="16" tileheight="16" tilecount="96" columns="12">
+ <image source="Tilemap.png" width="200" height="128"/>
+ <tile id="24">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="-0.363636" y="-0.363636" width="16.3636" height="16.3636"/>
+   <object id="4" x="-0.363636" y="-0.363636" width="16.3636" height="16.3636"/>
+   <object id="5" x="7.63223" y="7.26033"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16">
+    <polygon points="0,0 16,0 16,-8.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="7">
+    <polygon points="0,0 15.9375,-7 16,9 0,9"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
