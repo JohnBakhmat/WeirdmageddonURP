70d3d1d794707111580ee4f0f70ac8c2dfa97916
John Bakhmat <yevhenii.bakhmat@nure.ua>
2023-03-01 12:19:32 +0300

    Updated level tileset

--- Assets/Tilemaps/Example.tsx
+++ Assets/Tilemaps/Example.tsx
@@ -1,26 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Example" tilewidth="16" tileheight="16" tilecount="96" columns="12">
- <image source="Tilemap.png" width="200" height="128"/>
- <tile id="24">
+<tileset version="1.9" tiledversion="1.9.2" name="Example" tilewidth="16" tileheight="16" tilecount="144" columns="18">
+ <image source="Tilemap.png" width="299" height="128"/>
+ <tile id="36">
   <objectgroup draworder="index" id="3">
    <object id="2" x="-0.363636" y="-0.363636" width="16.3636" height="16.3636"/>
    <object id="4" x="-0.363636" y="-0.363636" width="16.3636" height="16.3636"/>
    <object id="5" x="7.63223" y="7.26033"/>
   </objectgroup>
  </tile>
- <tile id="28">
+ <tile id="40">
   <objectgroup draworder="index" id="2">
    <object id="4" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
- <tile id="69">
+ <tile id="99">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="16">
     <polygon points="0,0 16,0 16,-8.125"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="70">
+ <tile id="100">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="7">
     <polygon points="0,0 15.9375,-7 16,9 0,9"/>
